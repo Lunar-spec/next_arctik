4af9d20b6d6c0b767205370bb134727c55744124
Manjesh Singh <smanjesh1854@gmail.com>
2024-02-14 12:40:03 +0530

    added: checkout

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,8 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model";
 import { Button } from "../ui/button";
-
-// import { loadStripe } from "@stripe/stripe-js";
-// import { loadStripe } from "@stripe/stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import { useEffect } from "react";
 import { checkoutOrder } from "@/lib/actions/order.actions";
 
@@ -10,7 +8,6 @@ loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
 const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
   useEffect(() => {
-    // Check to see if this is a redirect back from Checkout
     const query = new URLSearchParams(window.location.search);
     if (query.get("success")) {
       console.log("Order placed! You will receive an email confirmation.");
@@ -18,7 +15,7 @@ const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
 
     if (query.get("canceled")) {
       console.log(
-        "Order canceled -- continue to shop around and checkout when you’re ready."
+        "Order canceled -- continue to shop around and checkout when you're ready."
       );
     }
   }, []);
