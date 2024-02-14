7f9389a1bbc2e646bbec1c34ef01c0a5b8e7ca2d
Manjesh Singh <smanjesh1854@gmail.com>
2024-02-14 12:54:59 +0530

    added: stripe

--- lib/actions/order.actions.ts
+++ lib/actions/order.actions.ts
@@ -1,6 +1,6 @@
 "use server"
 import Stripe from 'stripe'
-import { CheckoutOrderParams } from "@/types"
+import { CheckoutOrderParams, CreateOrderParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import Order from "../database/models/order.model"
@@ -36,5 +36,20 @@ export const checkoutOrder = async (order: CheckoutOrderParams) => {
         redirect(session.url!)
     } catch (error) {
         throw error;
+    }
+}
+
+export const createOrder = async (order: CreateOrderParams) => {
+    try {
+        await connectToDatabase();
+
+        const newOrder = await Order.create({
+            ...order,
+            event: order.eventId,
+            buyer: order.buyerId
+        })
+        return JSON.parse(JSON.stringify(newOrder));
+    } catch (error) {
+        handleError(error)
     }
 }
